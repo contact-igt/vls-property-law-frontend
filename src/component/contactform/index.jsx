3c86af4e4786472c0355120972b8c786d7c9fe44
Veeravel-S <veeravel.igt@gmail.com>
2026-05-22 14:50:29 +0530

    changes

--- src/component/contactform/index.jsx
+++ src/component/contactform/index.jsx
@@ -71,7 +71,7 @@ const ContactForm = ({
     const resp = await fetch("/api/create-order", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ amount: HomePage?.razorpay?.amount }),
+      body: JSON.stringify({ amount: 1 }),
     });
 
     const order = await resp.json();
@@ -144,7 +144,7 @@ const ContactForm = ({
         // );
 
 
-        // await registerUserToDB(apiPayload);
+        await registerUserToDB(apiPayload);
 
         const params = new URLSearchParams();
         Object.keys(apiPayload).forEach((key) =>
